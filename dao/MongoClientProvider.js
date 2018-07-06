fa9b0184af9d8072fa395004aa2dec969100c623
abhishek <abhishek@redmart.com>
2018-07-06 23:03:57 +0700

    add cloud mongo db (mLab) for deployment.

--- dao/MongoClientProvider.js
+++ dao/MongoClientProvider.js
@@ -2,7 +2,7 @@ class MongoClientProvider {
 
   getMongoClient () {
     const MongoClient = require('mongodb').MongoClient
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb://userProfileService:User2018@ds227110.mlab.com:27110/user-profiling-system'
     const mongoClient = MongoClient.connect(url, {useNewUrlParser: true})
     return mongoClient
   }
