f4928b023adcc0a8cd475b13c3f2ee38e7839a67
abhishek <abhishek@redmart.com>
2018-07-06 22:20:34 +0700

    Add swagger dependency

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ const userRouter = require('./routes/User')
 const healthRouter = require('./routes/Health')
 const config = require('config')
 const applicationPort = config.get('applicationPort')
+const swaggerUi = require('swagger-ui-express')
+//const swaggerDocument = require('./swagger.json')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerDefinition = {
+  info: {
+    title: 'Node Swagger API',
+    version: '1.0.0',
+    description: 'API documentation for User Profiling System',
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+};
+const swaggerOptions = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./server.js'],// pass all in array
+}
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+const port = 3000
+const healthEndPointPath = "/health"
+const userRegistrationEndPointPath = "/register"
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -19,6 +41,10 @@ app.factory('mongoClientPromise', (req, res, next) => {
 
 app.use('/v1', healthRouter)
 app.use('/v1', userRouter)
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.send(swaggerSpec)
+})
 
 app.listen(process.env.PORT || applicationPort, () => console.log(`user profiling service started at ${applicationPort}`))
 
